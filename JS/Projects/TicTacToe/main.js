dc409d48cdb50cd2e2cae4b50a8ac0b4d09ac7b9
Krishna <kaiavi71@gmail.com>
2024-01-03 20:53:46 -0500

    ds

--- JS/Projects/TicTacToe/main.js
+++ JS/Projects/TicTacToe/main.js
@@ -1,61 +1,68 @@
 const cells = document.querySelectorAll(".cellIndex");
 const status = document.querySelector("#status");
 const result = document.querySelector("#result");
+const restart = document.getElementById('restart');
+const reset = document.getElementById('scorereset');
+const turn = document.getElementById('status');
+const x = document.getElementById('1');
+const y = document.getElementById('2');
+let sum1 = 0;
+let sum2 = 0;
 const winCondition = [
-    // [0,1,2],
-    // [3,4,5],
-    // [6,7,8],
-    // [0,3,6],
-    // [1,4,7],
-    // [2,5,8],
-    // [0,4,8],
-    // [2,4,6]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 
-    [
-        1,1,1,
-        0,0,0,
-        0,0,0
-    ],
-    [
-        0,0,0,
-        1,1,1,
-        0,0,0
-    ],
-    [
-        0,0,0,
-        0,0,0,
-        1,1,1
-    ],
-    [
-        1,0,0,
-        1,0,0,
-        1,0,0
-    ],
-    [
-        0,1,0,
-        0,1,0,
-        0,1,0
-    ],
-    [
-        0,0,1,
-        0,0,1,
-        0,0,1
-    ],
-    [
-        1,0,0,
-        0,1,0,
-        0,0,1
-    ],
-    [
-        0,0,1,
-        0,1,0,
-        1,0,0
-    ],
+    // [
+    //     1,1,1,
+    //     0,0,0,
+    //     0,0,0
+    // ],
+    // [
+    //     0,0,0,
+    //     1,1,1,
+    //     0,0,0
+    // ],
+    // [
+    //     0,0,0,
+    //     0,0,0,
+    //     1,1,1
+    // ],
+    // [
+    //     1,0,0,
+    //     1,0,0,
+    //     1,0,0
+    // ],
+    // [
+    //     0,1,0,
+    //     0,1,0,
+    //     0,1,0
+    // ],
+    // [
+    //     0,0,1,
+    //     0,0,1,
+    //     0,0,1
+    // ],
+    // [
+    //     1,0,0,
+    //     0,1,0,
+    //     0,0,1
+    // ],
+    // [
+    //     0,0,1,
+    //     0,1,0,
+    //     1,0,0
+    // ],
 ];
-const cellValue = [0,0,0,0,0,0,0,0,0];
+// const cellValue = [0,0,0,0,0,0,0,0,0];
 let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = 'X';
-let running = false;
+let running = true;
 
 function cellClicked() {
     cells.forEach(cell => {
@@ -64,89 +71,97 @@ function cellClicked() {
             console.log(cellIndex);
 
             options[cellIndex] = currentPlayer;
-            console.log(options);
+            cells.innerHTML = options[cellIndex];
 
-            if(currentPlayer === 'X'){
-                cellValue[cellIndex]=1;
-                console.log(cellValue);
-            }        
-
-            switchPlayer();
+            if(!running){
+                return;
+            }
+            // if(currentPlayer === 'X'){
+            //     cellValue[cellIndex]=1;
+            //     console.log(cellValue);
+            // }        
             checkWinner();
+            switchPlayer();
+            restartGame();
+            
+            
         });
     });
 }
 
 function switchPlayer(){
-    switch(currentPlayer){
-        case 'X':
-            currentPlayer = 'O';
-            break;
-        default :
-            currentPlayer = 'X';
-            break;
+    if(running){
+        switch(currentPlayer){
+            case 'X':
+                currentPlayer = 'O';
+                break;
+            default :
+                currentPlayer = 'X';
+                break;
+        }
+
+        turn.innerHTML = `${currentPlayer}'s Turn`;
+        console.log(currentPlayer);
     }
-    console.log(currentPlayer);
 }
 
 function checkWinner() {
-    for (let condition of winCondition) {
-        let isWinner = true;
+    for(let i = 0; i<=7; i++){
+        const condition = winCondition[i];
+        let a = options[condition[0]];
+        let b = options[condition[1]];
+        let c = options[condition[2]];
+        console.log(a);
+        console.log(b);
+        console.log(c);
 
-        for (let i = 0; i < condition.length; i++) {
-            if (cellValue[i] !== condition[i]) {
-                isWinner = false;
-                break;
-            }
+        if(a === '' || b === '' || c === ''){
+            continue;
         }
-
-        if (isWinner) {
-            console.log("Win!!!");
-            running = true; // Optionally, set running to true to prevent further moves
+        if(a === b && b === c){
+            running = false;
+            result.innerHTML = `${currentPlayer} Wins!!!`;
+            scoreBoard(currentPlayer);
             break;
-        }
+        }   
+        if(!options.includes("")){
+            running = false;
+            result.innerHTML = "Draw!!!";
+            break;
+        }        
     }
 }
 
+function restartGame(){
+    restart.addEventListener('click', function(){
+        options = ["", "", "", "", "", "", "", "", ""];
+        currentPlayer = 'X';
+        running = true;
+        turn.innerHTML = `X's Turn`;
+        result.innerHTML = ``;
+    })
+}
+
+function scoreBoard(score){
+    if(score == 'X'){
+        sum1++;
+        x.innerHTML = sum1;
+    }
+    if(score == 'O'){
+        sum2++;
+        y.innerHTML  = sum2;
+    }
+}
+
+function resetScore(){
+    reset.addEventListener('click', function(){
+        sum1 = 0;
+        sum2 = 0;
+        x.innerHTML = sum1;
+        y.innerHTML = sum2;
+        
+    })
+}
+
+resetScore()
 cellClicked();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function updateCell(cell, cellIndex){
-    
-// }
-
-// function changePlayer(){
-
-// }
-
-// function checkWinner(){
-
-// }
-
-// function restartGame(){
-
-// }
-
-// function InitializeGame(){
-
-// }
-
