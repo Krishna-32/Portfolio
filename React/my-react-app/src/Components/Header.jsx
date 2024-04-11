8aa05ccf3d55f7e683d01a55893c1a71ab59c92e
Krishna <kaiavi71@gmail.com>
2024-04-11 08:35:00 -0400

    b

---
+++ React/my-react-app/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <header>
+            <h1>Crypto Tracker</h1>
+
+            <ul>
+                <li>Mango</li>
+                <li>Guava</li>
+                <li>Apple</li>
+                <li>Cherry</li>
+                <li>Coconut</li>
+            </ul>
+
+            <hr/>
+        </header>
+    );
+}
+
+export default Header
