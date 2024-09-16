6576db89242a3cc9b6fa4eb62371014e72dc0912
Krishna <kaiavi71@gmail.com>
2024-09-16 18:30:02 -0400

    scrolling animation

--- Projects/bonjovi/src/App.jsx
+++ Projects/bonjovi/src/App.jsx
@@ -1,38 +1,77 @@
-import React from 'react'
-import Home from './modules/Home'
-import Bg from './components/Bg'
-import Navbar from './components/Navbar'
-import Test1 from './Test1'
-import Test2 from './Test2'
+import React, { useRef, useEffect } from 'react';
+import Home from './modules/Home';
+import Bg from './components/Bg';
+import Navbar from './components/Navbar';
+import Test1 from './Test1';
+import Test2 from './Test2';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  const innerDiv = useRef(null);
+  const pin = useRef(null); // Reference for Test1 component
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      // Pin the pin div (Test1)
+      ScrollTrigger.create({
+        trigger: pin.current,
+        start: "top top",
+        end: "+=1000", // Adjust to control how long it stays pinned
+        pin: true,
+        scrub: true,
+        markers: true // For debugging; you can remove later
+      });
+
+      // Scroll through innerDiv while pinning
+      gsap.to(innerDiv.current, {
+        yPercent: -100, // Moves the inner div up while scrolling
+        ease: "none",
+        scrollTrigger: {
+          trigger: pin.current,
+          start: "top top",
+          end: "+=1000", // Same end value as pinning
+          scrub: 1,
+        },
+      });
+    });
+
+    return () => ctx.revert(); // Cleanup on unmount
+  }, []);
 
   return (
     <>
- this is test
-    <div className='wrapper'>
+      <div className='wrapper'>
+        <Navbar className="fixed z-[99] w-full top-0" />
+        <div>
+          <Home />
+        </div>
+        <div>
+          <Bg color='blackk' bgcolor='whitee' title='Bon Jovi' date='1983 - 1984' />
+        </div>
+        <div>
+          <Bg color='whitee' bgcolor='blackk' title='7800° Fahrenheit' date='1985 - 1986' />
+        </div>
+      
+        <div ref={pin} className='relative overflow-hidden'>
+          <Test1 />
+          
+          {/* The innerDiv you want to scroll through */}
+          <div ref={innerDiv} className='absolute w-full flex justify-center items-center uppercase font-formulab text-[10vw] flex-col gap-[50vw] top-full'>
+            <span>Hi</span>
+            <span>Hello</span>
+            <span>Amigos</span>
+            <span>Namaste</span>
+            <span>Ola</span>
+          </div>
+        </div>
 
-      <Navbar className="fixed z-[99] w-full top-0"/>
-
-      <div>
-        <Home/>
+        <Test2 />
       </div>
-
-      <div>
-        <Bg color='blackk' bgcolor='whitee' title='Bon jovi' date='1983 - 1984'/>
-      </div>
-
-      <div>
-        <Bg color='whitee' bgcolor='blackk' title='7800° fahrenhiet' date='1985 - 1986'/>
-      </div>
-
-      <Test1/>
-      <Test2/>
-
-    </div>
-
     </>
-    )
+  );
 }
 
-export default App
+export default App;
--- Projects/bonjovi/src/Test1.jsx
+++ Projects/bonjovi/src/Test1.jsx
@@ -9,41 +9,18 @@ function Test1() {
   const main = useRef();
   const innerDiv = useRef();
 
-  useEffect(() => {
-    // Pin the main div
-    ScrollTrigger.create({
-      trigger: main.current,
-      start: "top top", 
-      end: "max",
-      pin: true,
-      pinSpacing: false,
-      markers: true
-    });
-
-    // Animate the second div's Y translation
-    // gsap.to(innerDiv.current, {
-    //   top: 0, // Move the div to translateY(0)
-    //   scrollTrigger: {
-    //     trigger: main.current,
-    //     start: "top top", // Start animation when main div hits the top
-    //     end: "bottom top", // End animation when the bottom of the main div hits the top
-    //     scrub: true, // Smooth scrolling effect
-    //     markers: false, // Debugging tool
-    //   },
-    // });
-  }, []);
 
   return (
-    <div ref={main} className='relative w-full h-screen bg-pink-300 flex justify-center items-center uppercase font-formulab text-[10vw]'>
+    <div ref={main} className='relative w-full h-screen bg-pink-300 flex justify-center items-center uppercase font-formulab text-[10vw] overflow-hidden'>
       Test1
 
-      {/* <div ref={innerDiv} className='absolute w-full bg-green-300 flex justify-center items-center uppercase font-formulab text-[10vw] flex-col gap-[50vw] top-full'>
+      <div ref={innerDiv} className='absolute w-full bg-green-300 flex justify-center items-center uppercase font-formulab text-[10vw] flex-col gap-[50vw] top-full'>
         <span>Hi</span>
         <span>Hello</span>
         <span>Amigos</span>
         <span>Namaste</span>
         <span>Ola</span>
-      </div> */}
+      </div>
     </div>
   );
 }
