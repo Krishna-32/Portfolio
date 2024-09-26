b73ee38bf2b0a3818530a082d88ca9431d7321c4
Krishna <kaiavi71@gmail.com>
2024-09-26 14:33:46 -0400

    initial render animation

--- Projects/portfolio/src/modules/Hero.jsx
+++ Projects/portfolio/src/modules/Hero.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Hero() {
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const mm = gsap.matchMedia();
+    const ctx = gsap.context(() => {
+      mm.add({
+         isMobile: "(max-width: 1023px)",
+         isDesktop: "(min-width: 1024px)"
+      }, (context) => {
+        let {isMobile, isDesktop} = context.conditions;
+        if(isDesktop){
+          gsap.from('.first', {
+            x: '-10vw',
+            opacity: 0,
+            duration: 1,
+            ease: 'stepped.inOut',
+          });
+          gsap.from('.second', {
+            x: '37vw',
+            opacity: 0,
+            duration: 1,
+            ease: 'stepped.inOut',
+          });
+          gsap.from('.third', {
+            x: '-10vw',
+            opacity: 0,
+            duration: 1,
+            ease: 'stepped.inOut',
+          });
+        }
+
+        if(isMobile){
+          gsap.from(containerRef.current, {
+            opacity: 0,
+            duration: 1,
+            ease: 'sine.inOut',
+          });
+        }
+      });
+    });
+
+    
+
+    return () => ctx.revert(); // Cleanup on unmount
+  }, [containerRef]);
+
+  
+
   return (
-    <div className='wrapper h-[60vh] lg:h-screen w-full bg-blackk flex justify-center items-center'>
+    <div ref={containerRef} className='wrapper h-[60vh] lg:h-screen w-full bg-blackk flex justify-center items-center'>
       <div className='flex flex-col justify-center h-1/2 lg:h-full font-seasonr uppercase text-[16vw] tracking-[-.2rem] leading-[14vw] lg:leading-[12.2vw]  text-whitee overflow-visible'>
-        <span className='overflow-visible lg:-translate-x-[5vw]'>Welcome</span>
-        <span className='overflow-visible translate-x-[30vw]'>to my</span>
-        <span className='overflow-visible lg:-translate-x-[5vw]'>Studio</span>
+        <span className='first overflow-visible lg:-translate-x-[5.5vw]'>Welcome</span>
+        <span className='second overflow-visible translate-x-[33.5vw]'>to my</span>
+        <span className='third overflow-visible lg:-translate-x-[5.5vw]'>Studio</span>
       </div>
     </div>
   )
--- Projects/portfolio/src/modules/Work.jsx
+++ Projects/portfolio/src/modules/Work.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import InnerDiv from '../components/InnerDiv'
+import { gsap } from "gsap";
 
 function Work() {
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const mm = gsap.matchMedia();
+    const ctx = gsap.context(() => {
+      mm.add("(max-width: 1023px)", () => {
+        gsap.from(containerRef.current, {
+          opacity: 0,
+          duration: 1,
+          ease: 'sine.inOut',
+        });
+      });
+    });
+
+    
+
+    return () => ctx.revert(); // Cleanup on unmount
+  }, [containerRef]);
+
   return (
-    <div className='lg:my-20'>
+    <div ref={containerRef} className='lg:my-20'>
         <InnerDiv idx='01' title='Gucci clone' />
         <InnerDiv idx='02' title='Bonjovi' />
         <InnerDiv idx='03' title='Gucci clone' />
