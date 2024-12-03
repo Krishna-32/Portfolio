613af8067e17aa9601a35614af602eb610fafa0e
Krishna <kaiavi71@gmail.com>
2024-12-03 18:51:55 -0500

    .

---
+++ Next/Projects/pintrest/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectToDB from "../../../libs/mongodb";
+import User from "../../../models/users";
+import bcrypt from "bcrypt";
+
+const authOPtions = {
+ providers: [
+   GoogleProvider({ 
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+   }),
+   GithubProvider({
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+   }),
+   CredentialsProvider({
+    name: "Credentials",
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "Username" },
+      password: { label: "Password", type: "password", placeholder: "Password" },
+    },
+    async authorize(credentials) {
+      connectToDB();
+
+      const user = await User.findOne({ username: credentials.username });
+
+      if (!user) throw new Error("User not found");
+
+      const passwordsMatch = await bcrypt.compare(credentials.password, user.password);
+
+      if (!passwordsMatch) throw new Error("Invalid credentials");
+
+      return {
+       username: user.username,
+       email: user.email,
+       image: user.image,
+      }
+    }
+   })
+ ],
+ pages: {
+  signIn: "/login",
+ },
+}
+
+const handler = NextAuth(authOPtions);
+
+export { handler as GET, handler as POST };
+
+
---
+++ Next/Projects/pintrest/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full h-16 bg-white'></div>
+  )
+}
+
+export default Navbar
